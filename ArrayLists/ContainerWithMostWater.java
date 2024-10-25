09391ccae7cd487de7ffb5a8037c84d5f7e3b6c0
anishghanwat <anishghanwat.work@gmail.com>
2024-10-26 00:38:07 +0530

    ArrayList Q1

--- ArrayLists/ContainerWithMostWater.java
+++ ArrayLists/ContainerWithMostWater.java
@@ -2,14 +2,34 @@ import java.util.ArrayList;
 
 public class ContainerWithMostWater {
     public static int maxArea(ArrayList<Integer> height) {
-        int maxWater = 0;
+        // Brute force - O(n^2)
+        // int maxWater = 0;
 
-        for (int i = 0; i < height.size(); i++) {
-            for (int j = i + 1; j < height.size(); j++) {
-                int ht = Math.min(height.get(i), height.get(j));
-                int width = j - i;
-                int water = ht * width;
-                maxWater = Math.max(maxWater, water);
+        // for (int i = 0; i < height.size(); i++) {
+        // for (int j = i + 1; j < height.size(); j++) {
+        // int ht = Math.min(height.get(i), height.get(j));
+        // int width = j - i;
+        // int water = ht * width;
+        // maxWater = Math.max(maxWater, water);
+        // }
+        // }
+        // return maxWater;
+
+        // 2 Pointer Approch - O(n)
+        int maxWater = 0;
+        int lp = 0;
+        int rp = height.size() - 1;
+
+        while (lp < rp) {
+            int ht = Math.min(height.get(lp), height.get(rp));
+            int width = rp - lp;
+            int water = ht * width;
+            maxWater = Math.max(maxWater, water);
+
+            if (height.get(lp) < height.get(rp)) {
+                lp++;
+            } else {
+                rp--;
             }
         }
         return maxWater;
