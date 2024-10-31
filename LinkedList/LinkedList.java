fde044e1d805b9a17d730db16ade6bfa6be41b15
anishghanwat <anishghanwat.work@gmail.com>
2024-11-01 02:58:08 +0530

    linked list final

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -11,10 +11,14 @@ public class LinkedList {
 
     public static Node head;
     public static Node tail;
+    public static int size; // O(1) Constant
 
+    // O(1) Constant
     public void addFirst(int data) {
         // Step 1: create new Node
         Node newNode = new Node(data);
+
+        size++;
 
         // if linked list is empty
         if (head == null) {
@@ -28,14 +32,49 @@ public class LinkedList {
         head = newNode;
     }
 
+    // O(n) linear
+    public void add(int index, int data) {
+        // Add at head
+        if (index == 0) {
+            addFirst(data);
+            return;
+        }
+
+        // Create new node
+        Node newNode = new Node(data);
+
+        size++;
+
+        // Initialization
+        Node temp = head;
+        int i = 0;
+
+        // Loop to find previous index
+        while (i < index - 1) {
+            temp = temp.next;
+            i++;
+        }
+
+        // links
+        // newNode to Index + 1
+        newNode.next = temp.next;
+        // previous to newNode
+        temp.next = newNode;
+    }
+
+    // O(1) Constant
     public void addLast(int data) {
         // Step 1: create new node
         Node newNode = new Node(data);
+
+        size++;
+
         // if linkedlist is empty
         if (head == null) {
             head = tail = newNode;
             return;
         }
+
         // Step 2: tail next = newNode
         tail.next = newNode;
 
@@ -43,6 +82,7 @@ public class LinkedList {
         tail = newNode;
     }
 
+    // O(n) Linear
     public void print() {
         // assign head vale to temp
         Node temp = head;
@@ -55,16 +95,114 @@ public class LinkedList {
         System.out.println("null");
     }
 
+    public int removeFirst() {
+        if (size == 0) {
+            System.out.println("Linked List is empty!");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int value = head.data;
+            head = tail = null;
+            size = 0;
+            return value;
+        }
+        int value = head.data;
+        head = head.next;
+        size--;
+        return value;
+    }
+
+    public int removeLast() {
+        if (size == 0) {
+            System.out.println("Linked List is empty!");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int value = tail.data;
+            head = tail = null;
+            size = 0;
+            return value;
+        }
+        Node temp = head;
+        for (int i = 0; i < size - 2; i++) {
+            temp = temp.next;
+        }
+        int value = tail.data;
+        temp.next = null;
+        tail = temp;
+        size--;
+        return value;
+    }
+
+    // O(n) Linear
+    public int iterativeSearch(int key) {
+        Node temp = head;
+        int i = 0;
+
+        while (temp != null) {
+            if (temp.data == key) {
+                return i;
+            }
+            temp = temp.next;
+            i++;
+        }
+        return -1;
+    }
+
+    // O(n) Linear
+    public int recursiveSearch(int key) {
+        return helper(head, key);
+    }
+
+    public int helper(Node head, int key) {
+        // base case
+        if (head == null) {
+            return -1;
+        }
+        // key found
+        if (head.data == key) {
+            return 0;
+        }
+        // recursive calls
+        int index = helper(head.next, key);
+        // key not found
+        if (index == -1) {
+            return -1;
+        }
+        // Index + 1
+        return index + 1;
+    }
+
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         ll.print();
+        // Add First
         ll.addFirst(2);
         ll.print();
         ll.addFirst(1);
         ll.print();
+
+        // Add Last
         ll.addLast(3);
         ll.print();
         ll.addLast(4);
         ll.print();
+
+        // Add Middle
+        ll.add(2, 9);
+        ll.print();
+
+        // Remove First
+        ll.removeFirst();
+        ll.print();
+
+        // Remove Last
+        ll.removeLast();
+        ll.print();
+        System.out.println("Size of LL: " + ll.size);
+
+        // Iterative Search
+        System.out.println("Found by Iterative Search: " + ll.iterativeSearch(3));
+
+        // Recursive Search
+        System.out.println("Found by Recursive Search: " + ll.recursiveSearch(3));
     }
 }
